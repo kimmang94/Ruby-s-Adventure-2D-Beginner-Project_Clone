2743c3a0061fea469f57efa5a8aab1691e691db0
kimhyeok <kimmang940715@naver.com>
2023-09-23 23:16:23 +0900

    타일맵 생성

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -7,12 +7,20 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //QualitySettings.vSyncCount = 0;
+        // 프레임 조정하기 
+        //Application.targetFrameRate = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f + horizontal * Time.deltaTime;
+        position.y = position.y + 0.1f + vertical * Time.deltaTime;
+        transform.position = position;
     }
 }
